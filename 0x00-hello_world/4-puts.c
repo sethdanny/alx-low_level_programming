7cdb3322d340d12b08992f2e9c720906f58f2c54
sethdanny <naddulidaniel1994@gmail.com>
2022-09-08 07:01:30 -0700

    added task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - prints a statment using puts
  * Return: 0 if executed properly, non zero otherwise
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle \n");
+return (0);
 }
