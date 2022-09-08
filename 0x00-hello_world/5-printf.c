0bd2cf881b5a3e78bf07f62a6d9038c4ebee6e76
sethdanny <naddulidaniel1994@gmail.com>
2022-09-08 07:07:34 -0700

    added task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print a statement using printf function
+ * Return: 0 on success when compiled properly, non zero otherwise
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
