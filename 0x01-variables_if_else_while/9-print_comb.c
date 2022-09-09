bb1ce8f40ad7131de70a15bd90d0904dd63395d4
sethdanny <naddulidaniel1994@gmail.com>
2022-09-09 10:18:47 -0700

    added task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - print numbers seperated by comma
+  * Return: 0 Always success
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
