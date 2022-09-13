ca4e93604a778d7541895b51eb81defdfed560a3
sethdanny <naddulidaniel1994@gmail.com>
2022-09-13 04:01:44 -0700

    added task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - print a string putchar
+  * Return: 0 (Always success)
+  */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
