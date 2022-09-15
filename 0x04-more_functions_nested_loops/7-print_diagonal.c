ff15c5bddef845e67730630abd21d3ef2d1c3d97
sethdanny <naddulidaniel1994@gmail.com>
2022-09-15 04:11:45 -0700

    fixed diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,9 +6,24 @@
   */
 void print_diagonal(int n)
 {
-	char c;
+	int i, j;
 
-	for (c = 0; c < n; c++)
-		_putchar('/');
-	_putchar('\n');
+	if (n <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (i == j)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
 }
