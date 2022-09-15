b44bbc4fd7a802a831130c91a46d94300987a233
sethdanny <naddulidaniel1994@gmail.com>
2022-09-15 06:29:30 -0700

    added task 1

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,28 +9,24 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
-	{
+	for (i = 1; i < 100; i++)
 		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
 		else
 		{
 			printf("%d ", i);
 		}
-		if (i != 100)
-			putchar(' ');
-		else
-			putchar('\n');
-	}
+	printf("Buzz");
+	printf("\n");
 	return (0);
 }
