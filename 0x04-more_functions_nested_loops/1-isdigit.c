e540ee310e0f512754b735f500515acc8b9cff0e
sethdanny <naddulidaniel1994@gmail.com>
2022-09-15 01:30:34 -0700

    multiply a with b

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * _isdigit - checks for a digit from 0 to 9
+  * @c: int to check
+  * Return: 1 if c is digit, 0 otherwise
+  */
+int _isdigit(int c)
+{
+	return (c >= 48 && c < 58);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * mul - multiplies two numbers
+  * @a: int to multiply with b
+  * @b: int to multiply with a
+  * Return: a times b
+  */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
