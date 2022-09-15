6346ecbfdd2e2cc27303af2c552f11074d040bfb
sethdanny <naddulidaniel1994@gmail.com>
2022-09-15 01:43:05 -0700

    added task 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - computes numbers
+  * Return: 0 Always (success)
+  */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - print numbers except 2 and 4
+  * Return: Always 0 (success)
+  */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (n == 2 && n == 4)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+
+	return (0);
+}
