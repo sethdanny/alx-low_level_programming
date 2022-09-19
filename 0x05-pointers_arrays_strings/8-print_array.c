75d4e87906c20ba5ad37db2aea03069237c0b843
sethdanny <naddulidaniel1994@gmail.com>
2022-09-20 01:47:20 +0300

    added task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_array - print array elements seperated by comma and space
+  * @a: array name
+  * @n: number of elements in the array
+  */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf(", %d", a[i]);
+		}
+	}
+		printf("\n");
+}
