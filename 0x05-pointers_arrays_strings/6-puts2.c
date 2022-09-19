a9447b90c294de334d910e6c3efc2db74786f53a
sethdanny <naddulidaniel1994@gmail.com>
2022-09-20 01:18:34 +0300

    added task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * puts2 - print characters
+  * @str: string to print
+  */
+void puts2(char *str)
+{
+	int i, len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
