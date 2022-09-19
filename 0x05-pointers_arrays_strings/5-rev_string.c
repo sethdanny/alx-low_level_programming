3008141da74714e8797cc8dce4ed43dda1c1648a
sethdanny <naddulidaniel1994@gmail.com>
2022-09-20 00:58:23 +0300

    added task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * rev_string - reverse a string
+  * @s: string to be reversed
+  */
+void rev_string(char *s)
+{
+	char ch;
+	int i, j, len;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		ch = s[i];
+		s[i] = s[len1];
+		s[len1--] = ch;
+	}
+}
