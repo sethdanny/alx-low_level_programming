7973d14928dacebd527cef6fdd5a33ba60ba0ee2
sethdanny <naddulidaniel1994@gmail.com>
2022-09-30 14:28:19 +0300

    fixed unused parameter

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: arguments passed during execution
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
