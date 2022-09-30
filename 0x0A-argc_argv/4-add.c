c849f06d30ac9e769998b30530606852294918df
sethdanny <naddulidaniel1994@gmail.com>
2022-09-30 17:48:34 +0300

    added task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include <ctype.h>
 
 /**
- * main - print product of argument numbers
- * @argc: argument counter
- * @argv: numbers to multiply
- * Return: 0 on success, 1 if two arguments not given
+ * main - Print result of adding given arguments
+ * @argc: Number of arguments
+ * @argv: Arguments recieved
+ *
+ * Return: 0 on success, 1 if theres a nondigit arg
  */
-
 int main(int argc, char *argv[])
 {
-int j;
-int i = 1;
-int sum = 0;
+	int sum;
+	int count;
+	int i;
 
-if (argc == 0)
-{
-printf("0\n");
-return (0);
-}
-
-
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
-{
-printf("Error\n");
-return (1);
-}
-}
-sum += atoi(argv[i]);
-}
-
-printf("%d\n", sum);
-
+	count = 1;
+	sum = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (count < argc)
+	{
+		for (i = 0; argv[count][i] != '\0'; i++)
+		{
+			if (!(isdigit(argv[count][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[count]);
+		count++;
+	}
+	printf("%d\n", sum);
 	return (0);
 }
