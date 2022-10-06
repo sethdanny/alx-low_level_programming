ce8a9b6be71080b204fbbd1ef846d831d7a3c3b5
sethdanny <naddulidaniel1994@gmail.com>
2022-10-06 12:43:13 +0300

    fixed string 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len = 0;
 
-	for (i = 0; i < s[i]; i++)
+	for (i = 0; i < s1[i]; i++)
 		concat[len++] = s[i];
 
 	for (i = 0; s2[i] && i < n; i++)
