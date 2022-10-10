5e903b9e66c49e83f221eaee82cd153a2d8f076c
sethdanny <naddulidaniel1994@gmail.com>
2022-10-10 19:35:06 +0300

    added task 1

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * Return: the name of the program it was compiled from
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
