24b3115853b51be2ca522b76c120136677da5186
sethdanny <naddulidaniel1994@gmail.com>
2022-10-13 13:25:35 +0300

    added task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,12 +18,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		if (!separator)
 			printf("%d", va_arg(list, int));
+
+		else if (separator == NULL)
+			return;
+
 		else if (separator && i == 0)
 			printf("%d", va_arg(list, int));
 		else
-		{
 			printf("%s%d ", separator, va_arg(list, int));
-		}
 	}
 
 	va_end(list);
