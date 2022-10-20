1361adad5a950453e13541d22573d8adadf789e6
sethdanny <naddulidaniel1994@gmail.com>
2022-10-20 16:49:06 +0300

    added task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_list - print all elements in list_t
+ * @h: pointer to the node
+ * Return: - number of elements
+ */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+
+	return (s);
+}
