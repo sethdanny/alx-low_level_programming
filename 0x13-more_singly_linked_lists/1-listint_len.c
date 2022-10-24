750263a4bba809ea6ec2c993c561a727e23fcaad
sethdanny <naddulidaniel1994@gmail.com>
2022-10-24 17:49:30 +0300

    added task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints the length of a linked list
+ * @h: number of nodes
+ * Return: the size of the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
