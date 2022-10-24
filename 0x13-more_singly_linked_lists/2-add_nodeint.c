bf448507257aab61f274280797d261f1458f1bb1
sethdanny <naddulidaniel1994@gmail.com>
2022-10-25 01:01:51 +0300

    fixed error

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,9 +8,11 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *ptr = malloc(sizeof(listint_t));
+	listint_t *ptr;
 
-	if(!ptr)
+	ptr = malloc(sizeof(listint_t));
+
+	if (!ptr)
 		return (NULL);
 
 	ptr->n = n;
