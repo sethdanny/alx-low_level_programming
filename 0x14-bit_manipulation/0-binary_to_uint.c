cedcf649a0a1b0b225816f3657f80c82cdc1a55d
sethdanny <naddulidaniel1994@gmail.com>
2022-10-28 23:51:36 +0300

    added task 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary number to decimal
+ * @b: string to be converted
+ * Return: converted number to decimal
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int decval = 0;
+
+	if(!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+
+		if (b[i] < '0' && b[i] > '1')
+			return (0);
+		decval = 2 * decval + b[i] - '0';
+	}
+		return (decval);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+
+#endif
