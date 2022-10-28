f2aaf115834945978a065682e77d4576a436dabc
sethdanny <naddulidaniel1994@gmail.com>
2022-10-29 00:17:20 +0300

    added task 2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * binary_to_uint - convert binary number to decimal
- * @b: string to be converted
- * Return: converted number to decimal
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int decval = 0;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
 	for (i = 0; b[i]; i++)
 	{
-
-		if (b[i] < '0' && b[i] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		decval = 2 * decval + b[i] - '0';
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-		return (decval);
+
+	return (dec_val);
 }
